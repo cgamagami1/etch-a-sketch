50a345c4b3347bcee6ea673d6fb6878494418e80
cgamagami1 <81793604+cgamagami1@users.noreply.github.com>
2022-10-19 21:28:41 -0500

    Add paint and clear functionality
    -paint button now paints pixels the selected color
    -clear button sets all pixels to white

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const rainbowPaintButton = document.getElementById("rainbow-paint");
 const eraserButton = document.getElementById("eraser");
 const clearButton = document.getElementById("clear");
 const canvas = document.getElementById("canvas");
+const pixels = [];
 
-paintButton.addEventListener("click", () => {selectButton(paintButton)});
-rainbowPaintButton.addEventListener("click", () => {selectButton(rainbowPaintButton)});
-eraserButton.addEventListener("click", () => {selectButton(eraserButton)});
+paintButton.addEventListener("click", () => { selectButton(paintButton); });
+rainbowPaintButton.addEventListener("click", () => { selectButton(rainbowPaintButton); });
+eraserButton.addEventListener("click", () => { selectButton(eraserButton); });
 clearButton.addEventListener("click", clear);
 
 selectButton(paintButton);
@@ -15,7 +16,17 @@ selectButton(paintButton);
 for (let i = 0; i < 256; i++) {
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
+    pixel.addEventListener("mousedown", (e) => { selectPixel(e, pixel); });
+    pixel.addEventListener("mousemove", (e) => { selectPixel(e, pixel); });
     canvas.appendChild(pixel);
+    pixels.push(pixel);
+}
+
+function selectPixel(e, pixel) {
+    if(e.buttons == 1){
+        paintPixel(pixel);
+        e.preventDefault();
+    }
 }
 
 function selectButton(selectedButton) {
@@ -26,9 +37,11 @@ function selectButton(selectedButton) {
 }
 
 function paintPixel(pixel) {
-    
+    pixel.style.backgroundColor = colorInput.value;
 }
 
 function clear() {
-
+    for (pixel of pixels) {
+        pixel.style.backgroundColor = "white";
+    }
 }
